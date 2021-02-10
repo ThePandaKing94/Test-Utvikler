c9e93a79663e900732bf41824b535d3b9ee341b4
Eirik <eirikag1994@gmaail.com>
2021-02-10 22:49:27 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import BitCoin from './Components/BitCoin.js'
+import './App.css'
 
 function App() {
   return (
--- src/Components/BitCoin.js
+++ src/Components/BitCoin.js
@@ -1,32 +1,36 @@
-import React  from 'react'
+import React from 'react'
+import img from './bitcoin.jpg'
 export default class BitCoin extends React.Component {
-  
+
     state = {
         loading: true,
-        time: null,
+        time: true,
+        bpi: true,
+        USD: true,
+        rate_float: null,
+        updated: null,
     };
 
     async componentDidMount() {
         const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-        const respone = await fetch(url);
-        const data = await (respone).json();
-        this.setState({ time: null, loading: false});
-        console.log([data]);
+        const respone = fetch(url);
+        const data = await (await respone).json();
+        this.setState({ bpi: data.bpi.USD.rate_float });
+        this.setState({ loading: false });
+        this.setState({ updated: setInterval((data.time.updated), 1000) });
+        console.log(data);
     }
 
-    render(){
-        if(this.state.loading) {
-            return <div>loading...</div>
-        }
+    componentWillUnmount() {
+        clearInterval(this.interval);
+      }
 
-        if(!this.state.time) {
-            return <div>Registrerte ikke tiden</div>
-        }
-
-    return (
-        <div>
-              <div>{this.state.time}</div>
-        </div>
-    );
-}
+    render() {
+        return (
+            <div>
+                {this.state.loading ? <div>Loading...</div> : <div></div>}
+                {this.state.updated ? <div><img src={img} alt="bitcoin"/>{"At "+this.state.updated+" BitCoin's current value is: "+Math.round(this.state.bpi)+" USD"}</div> : <div>Something's wrong</div>}
+            </div>
+        );
+    }
 }
